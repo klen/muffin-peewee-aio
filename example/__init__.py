562c6d4a6be0383690e76b0530ba09fbecea8b1f
Kirill Klenov <horneds@gmail.com>
2021-09-07 15:09:44 +0300

    tests: fix

--- example/__init__.py
+++ example/__init__.py
@@ -5,14 +5,14 @@ import muffin_peewee
 
 
 # Setup application
-app = muffin.Application(
+app: muffin.Application = muffin.Application(
     'example',
 
     DEBUG=True,
     PEEWEE_CONNECTION='sqlite:///example.db',
     PEEWEE_MIGRATIONS_PATH='example/migrations',
 )
-db = muffin_peewee.Plugin(app)
+db: muffin_peewee.Plugin = muffin_peewee.Plugin(app)
 
 # Register views
 from example.views import *  # noqa
