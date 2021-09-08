f4bc01c8c0d3cf6fbd52126b7e27a9ccedafd90c
Kirill Klenov <horneds@gmail.com>
2021-09-08 17:31:06 +0300

    Bump version: 0.0.7 → 0.0.8

--- muffin_peewee/__init__.py
+++ muffin_peewee/__init__.py
@@ -12,7 +12,7 @@ from peewee_aio.manager import Manager, cached_property
 from .fields import JSONField, Choices
 
 
-__version__ = "0.0.7"
+__version__ = "0.0.8"
 __project__ = "muffin-peewee-aio"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
