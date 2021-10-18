d70bc1052be18d67572ca97246cbf2d6cd195a27
Kirill Klenov <horneds@gmail.com>
2021-10-18 19:01:50 +0300

    feat: python 3.10

--- setup.py
+++ setup.py
@@ -1,5 +1,31 @@
-#!/usr/bin/env python
+"""Setup the package."""
+
+
+# Parse requirements
+# ------------------
+import pkg_resources
+import pathlib
+
+
+def parse_requirements(path: str) -> 'list[str]':
+    with pathlib.Path(path).open() as requirements:
+        return [str(req) for req in pkg_resources.parse_requirements(requirements)]
+
+
+# Setup package
+# -------------
 
 from setuptools import setup
 
-setup()
+DRIVERS = ['asyncpg', 'aiopg', 'aiomysql', 'aiosqlite']
+
+
+setup(
+    install_requires=parse_requirements('requirements/requirements.txt'),
+    extras_require=dict(
+        {driver: [f"peewee-aio[{driver}]"] for driver in DRIVERS},
+        tests=parse_requirements('requirements/requirements-tests.txt'),
+    )
+)
+
+# pylama:ignore=E402,D
