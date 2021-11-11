e4dca79ec93082c2e8da6f244fc87c5a1463326a
Kirill Klenov <horneds@gmail.com>
2021-11-11 23:47:06 +0300

    Bump version: 0.4.0 → 0.4.1

--- muffin_peewee/__init__.py
+++ muffin_peewee/__init__.py
@@ -12,7 +12,7 @@ from peewee_aio.manager import Manager
 from .fields import JSONField, Choices
 
 
-__version__ = "0.4.0"
+__version__ = "0.4.1"
 __project__ = "muffin-peewee-aio"
 __author__ = "Kirill Klenov <horneds@gmail.com>"
 __license__ = "MIT"
