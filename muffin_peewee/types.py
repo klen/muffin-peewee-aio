4b3c50db400d3ea1c94878b17c92a79e5a26f59b
Kirill Klenov <horneds@gmail.com>
2023-02-15 21:20:32 +0700

    fix: wip

--- muffin_peewee/types.py
+++ muffin_peewee/types.py
@@ -2,5 +2,5 @@ from typing import Any, Callable, Dict, List, Tuple, Union
 
 TJSONDump = Callable[[Any], str]
 TJSONLoad = Callable[[str], Any]
-TChoice = Union[str, Tuple[str, Any]]
+TChoice = Union[str, Tuple[Any, str]]
 TChoices = Union[Dict[str, Any], List[TChoice]]
